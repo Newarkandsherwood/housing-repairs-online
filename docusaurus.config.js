1d0a1a827ddae9f1ca3564a4b321421945d26387
el <elena@madetech.com>
2022-02-28 11:09:30 +0000

    fix base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://city-of-lincoln-council.github.io',
+  baseUrl: '/housing-repairs-online/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
